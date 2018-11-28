cc55a1d9dfdb7fefbc26165fec6ec511fd5e4052
Areliez Vargas <areliez.vargas@gmail.com>
2018-11-28 12:59:32 -0400

    Refactor the test to generate a report

--- src/test/java/test/TestDashboard.java
+++ src/test/java/test/TestDashboard.java
@@ -1,11 +1,18 @@
 package test;
 
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import framework.pages.GoogleLandingPage;
+import framework.reports.ExtentManager;
 import org.testng.annotations.Test;
 
-public class TestDashboard {
+import java.lang.reflect.Method;
+
+public class TestDashboard extends TestBase{
     @Test
-    public void testGoogleLandingPage() {
+    public void testGoogleLandingPage(Method method) {
+
+        String methodName = method.getName();
         GoogleLandingPage googleLandingPage = new GoogleLandingPage();
 
         googleLandingPage
@@ -14,5 +21,8 @@ public class TestDashboard {
                 .clickOnSearchWithGoogleButton()
         .toolBar
             .clickOnImagesMenuItem();
+        ExtentTest logger = ExtentManager.getInstance().getExtendReport().startTest(methodName);
+        logger.log(LogStatus.PASS, "The test paso");
+        ExtentManager.getInstance().getExtendReport().endTest(logger);
     }
 }
--- src/test/java/test/TestDashboard2.java
+++ src/test/java/test/TestDashboard2.java
@@ -1,11 +1,18 @@
 package test;
 
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import framework.pages.GoogleLandingPage;
+import framework.reports.ExtentManager;
 import org.testng.annotations.Test;
 
-public class TestDashboard2 {
+import java.lang.reflect.Method;
+
+public class TestDashboard2 extends TestBase{
     @Test
-    public void testGoogleLandingPage() {
+    public void testGoogleLandingPageTwo(Method method) {
+        String methodName = method.getName();
+
         GoogleLandingPage googleLandingPage = new GoogleLandingPage();
 
         googleLandingPage
@@ -14,5 +21,8 @@ public class TestDashboard2 {
                 .clickOnSearchWithGoogleButton()
         .toolBar
             .clickOnImagesMenuItem();
+        ExtentTest logger = ExtentManager.getInstance().getExtendReport().startTest(methodName);
+        logger.log(LogStatus.PASS, "The test paso");
+        ExtentManager.getInstance().getExtendReport().endTest(logger);
     }
 }
