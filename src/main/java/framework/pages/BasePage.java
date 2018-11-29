6a8bad97306d87e004918c8a57e0b33869928669
Areliez Vargas <areliez.vargas@gmail.com>
2018-11-29 16:21:39 -0400

    Add constructor to BasePage

--- src/main/java/framework/pages/BasePage.java
+++ src/main/java/framework/pages/BasePage.java
@@ -1,13 +1,14 @@
 package framework.pages;
 
-import com.relevantcodes.extentreports.ExtentReports;
-import com.relevantcodes.extentreports.ExtentTest;
 import framework.manager.DriverManager;
-import framework.reports.ReportManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
 
 public class BasePage {
+
     protected WebDriver driver  = DriverManager.getInstance().getWebDriver();
-    protected ExtentReports report = ReportManager.getInstance().getExtendReport();
-    protected ExtentTest extentTest = ReportManager.getInstance().getExtentTest();
+
+    public BasePage(){
+        PageFactory.initElements(driver, this);
+    }
 }
