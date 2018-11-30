dea9672238309549483dadfe268061817be5d17a
Areliez Vargas <areliez.vargas@gmail.com>
2018-11-30 00:15:00 -0400

    Refactor testBase

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,20 +1,21 @@
 package test;
 
 import framework.reports.ReportManager;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 
 import java.lang.reflect.Method;
 
 public class TestBase {
 
     @BeforeMethod
-    public void setup (Method method) {
+    public void setup(Method method) {
         String nameMethod = method.getName();
         ReportManager.getInstance().startNewTest(nameMethod);
     }
 
     @AfterTest
-    public void tearDown () {
+    public void tearDown() {
         ReportManager.getInstance().endCurrentTest();
         ReportManager.getInstance().flushCurrentTest();
     }
