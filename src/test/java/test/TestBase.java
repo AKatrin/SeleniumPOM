038235d6bf869b46b1443933b6bde53230017462
Areliez Vargas <areliez.vargas@gmail.com>
2018-12-07 02:30:28 -0400

    Add atribute to get a string of a number random

--- src/test/java/test/TestBase.java
+++ src/test/java/test/TestBase.java
@@ -1,14 +1,18 @@
 package test;
 
+import com.github.mirreck.FakeFactory;
 import framework.pages.OrangeHRM.DashboardOrangeHRMPage;
 import framework.reports.ReportManager;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.lang.reflect.Method;
 
 public class TestBase extends LoginTest {
-
+    private FakeFactory fakeFactory= new FakeFactory();
+    protected String digits = fakeFactory.digits(3);
     @BeforeMethod
     public void setup(Method method) {
         String nameMethod = method.getName();
